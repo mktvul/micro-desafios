c6e5dc09fa205a77c70f4a24e349b518ea4c675f
mktvul <mkt.vul@gmail.com>
2021-09-27 23:14:43 -0300

    subiendo los archivos para la entrega del 28/09

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto 3000");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.post("/search", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.post ("/register", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
